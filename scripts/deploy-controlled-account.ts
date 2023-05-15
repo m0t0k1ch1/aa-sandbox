f53734253dc28aa0c73426492231c6376da9611c
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2023-05-15 18:19:53 +0900

    refactor: deploy scripts

--- scripts/deploy-controlled-account.ts
+++ scripts/deploy-controlled-account.ts
@@ -1,15 +1,18 @@
 import { ethers } from "hardhat";
 
-import { ControlledAccount } from "../typechain-types";
+import { ControlledAccount, Controller } from "../typechain-types";
 
 const ENTRY_POINT_ADDRESS = "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789";
 
 (async () => {
   const [owner] = await ethers.getSigners();
 
-  const controllerFactory = await ethers.getContractFactory("Controller");
-  const controller = await controllerFactory.deploy(ENTRY_POINT_ADDRESS);
-  await controller.deployed();
+  let controller: Controller;
+  {
+    const factory = await ethers.getContractFactory("Controller");
+    controller = await factory.deploy(ENTRY_POINT_ADDRESS);
+    await controller.deployed();
+  }
 
   let controlledAccount: ControlledAccount;
   {
--- scripts/deploy-tiny-account.ts
+++ scripts/deploy-tiny-account.ts
@@ -1,11 +1,16 @@
 import { ethers } from "hardhat";
 
+import { TinyAccount } from "../typechain-types";
+
 const ENTRY_POINT_ADDRESS = "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789";
 
 (async () => {
-  const tinyAccountFactory = await ethers.getContractFactory("TinyAccount");
-  const tinyAccount = await tinyAccountFactory.deploy(ENTRY_POINT_ADDRESS);
-  await tinyAccount.deployed();
+  let tinyAccount: TinyAccount;
+  {
+    const factory = await ethers.getContractFactory("TinyAccount");
+    tinyAccount = await factory.deploy(ENTRY_POINT_ADDRESS);
+    await tinyAccount.deployed();
+  }
 
   console.log(tinyAccount.address);
 })()
